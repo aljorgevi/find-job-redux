bf016d0dd806a0248a4cb065ad188043958d00b9
aljorgevi <jormencar@yahoo.com>
2022-06-11 15:26:42 +0100

    re-factor

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,5 +1,5 @@
-import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
 export default function PrivateRoute({ children }) {
 	const { user } = useSelector(store => store.user);
