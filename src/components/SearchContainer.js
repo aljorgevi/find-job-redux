c8721babf35927354be0591bdab1c9d25fe7d22a
aljorgevi <jormencar@yahoo.com>
2022-06-13 19:37:55 +0100

    add search container

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import { FormRow, FormRowSelect } from '.';
+import Wrapper from '../assets/wrappers/SearchContainer';
+import { useSelector, useDispatch } from 'react-redux';
+import { handleChange, clearFilters } from '../redux/features/allJobs/allJobsSlice';
 
 const SearchContainer = () => {
-	return <div>SearchContainer</div>;
-};
+	const dispatch = useDispatch();
+	const { isLoading, search, searchStatus, searchType, sort, sortOptions } = useSelector(
+		store => store.allJobs
+	);
+	const { jobTypeOptions, statusOptions } = useSelector(store => store.job);
 
+	const handleSearch = event => {
+		if (isLoading) return;
+		dispatch(handleChange({ name: event.target.name, value: event.target.value }));
+	};
+	const handleSubmit = event => {
+		event.preventDefault();
+		dispatch(clearFilters());
+	};
+
+	return (
+		<Wrapper>
+			<form className='form'>
+				<h4>search form</h4>
+				<div className='form-center'>
+					{/* search position */}
+					<FormRow type='text' name='search' value={search} handleChange={handleSearch} />
+					{/* search by status */}
+					<FormRowSelect
+						labelText='status'
+						name='searchStatus'
+						value={searchStatus}
+						handleChange={handleSearch}
+						list={['all', ...statusOptions]}
+					/>
+
+					{/* search by type*/}
+					<FormRowSelect
+						labelText='type'
+						name='searchType'
+						value={searchType}
+						handleChange={handleSearch}
+						list={['all', ...jobTypeOptions]}
+					/>
+					{/* sort */}
+					<FormRowSelect name='sort' value={sort} handleChange={handleSearch} list={sortOptions} />
+					<button className='btn btn-block btn-danger' disabled={isLoading} onClick={handleSubmit}>
+						clear filters
+					</button>
+				</div>
+			</form>
+		</Wrapper>
+	);
+};
 export default SearchContainer;
